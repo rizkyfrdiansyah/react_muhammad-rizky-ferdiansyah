287469f70c38f1301b3b424872464242e29d107a
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-09-10 16:52:56 +0700

    prob 1

--- 10_Introduction-Algorithms/praktikum/Problem1.js
+++ 10_Introduction-Algorithms/praktikum/Problem1.js
@@ -1,18 +1,11 @@
-function isPrime(n) {
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) {
-      return false;
-    }
-  }
-  return true;
-}
+let stats = [1.5, 2.56, 5.1, 12.33];
 
-function showPrimes(max) {
-  for (let i = 2; i <= max; i++) {
-    if (isPrime(i)) {
-      console.log(i);
-    }
-  }
-}
+stats = stats.map((stat) => Math.round(stat));
 
-showPrimes(100);
+console.log(stats);
+
+let temp = [];
+for (let i = 0; i < stats.length; i++) {
+  temp.push(Math.round(stats[i]));
+}
+console.log(temp);
