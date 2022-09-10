074a0a63564d073877a001bee82f368bf5a83a3b
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-09-10 16:54:34 +0700

    prob 2

--- 10_Introduction-Algorithms/praktikum/Problem2.js
+++ 10_Introduction-Algorithms/praktikum/Problem2.js
@@ -1,18 +1,21 @@
-function isPrime(n) {
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) {
-      return false;
-    }
-  }
-  return true;
-}
+let stats = [1.5, 2.56, 5.1, 12.33];
 
-function showPrimes(max) {
-  for (let i = 2; i <= max; i++) {
-    if (isPrime(i)) {
-      console.log(i);
-    }
-  }
-}
+stats = stats.map((stat) => Math.round(stat));
 
-showPrimes(100);
+// console.log(stats);
+
+let temp = [];
+for (let i = 0; i < stats.length; i++) {
+  temp.push(Math.round(stats[i]));
+}
+// console.log(temp);
+
+/**
+ * BUILT IN FUNCTION => MATH
+ *
+ * a. Rounding (Pembulatan)
+ * - Math.ceil()    => Pembulatan ke atas   Misal : Math.ceil(1.2) => 2
+ * - Math.floor()   => Pembulatan ke bawah  Misal : Math.floor(3.7) => 3
+ * - Math.round()   => Pembulatan mendekati angka di atas atau di bawah
+ * => Math.round(5.7) => 6, Math.round(1.2) => 1
+ */
