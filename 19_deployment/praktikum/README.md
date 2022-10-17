4c39f8ca2b2e2ec3c6d0d47b25a16629466d9e0f
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-10-18 01:03:36 +0700

    update task

--- 19_deployment/praktikum/README.md
+++ 19_deployment/praktikum/README.md
@@ -5,3 +5,13 @@ Deploylah code create react project yang sedikit diubah. Deploy menggunakan Surg
 # Contoh Tampilan
 
 Ini merupakan contoh tampilan create react project yang sedikit diubah. Kalian bisa menjadikan referensi. Bisa juga menyesuaikan selera teman teman. Perlu diketahui yang menjadi komponen utama penilaiannya ialah deployment, bukan tampilan create react project yang teman teman ubah.
+
+## LINK DEPLOYMENT: `taskDeploy-muhammadrizkyferdiansyah.surge.sh`
+
+## Successfully deployed
+
+![image](../screenshoots/Succes%20publish%20to%20deploy.png)
+
+## TAMPILAN
+
+![image](../screenshoots/hasil%20tampilan.png)
--- 19_deployment/src/App.js
+++ 19_deployment/src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Selamat Datang di Website Muhammad Rizky Ferdiansyah</p>
+        <a className="App-link" href="https://github.com/rizkyfrdiansyah/react_muhammad-rizky-ferdiansyah" target="_blank" rel="noopener noreferrer">
+          Github Saya
         </a>
       </header>
     </div>
